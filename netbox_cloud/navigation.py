4bb0bda7af0c8f8098f6573fb5a3b6dc241ea93a
emersonfelipesp <emerson@netdevopsbr.com>
2023-07-18 20:17:38 +0000

    Add top-level 'Cloud' navigation and first navigation buttons

--- netbox_cloud/navigation.py
+++ netbox_cloud/navigation.py
@@ -1,9 +1,22 @@
-from extras.plugins import PluginMenuButton, PluginMenuItem
+from extras.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 from utilities.choices import ButtonColorChoices
 
-menu_items = (
-    PluginMenuItem(
-        link="plugins:netbox_cloud:containers",
-        link_text="Containers",
-    ),
+proxmox_item = PluginMenuItem(
+    link='plugins:netbox_cloud:containers',
+    link_text='Containers',
 )
+
+vmware_item = PluginMenuItem(
+    link='plugins:netbox_cloud:containers',
+    link_text='Containers',
+)
+
+menu = PluginMenu(
+    label='Cloud',
+    groups=(
+        ('Proxmox', (proxmox_item,)),
+        ('Vmware', (vmware_item,)),
+    ),
+    icon_class='mdi mdi-cloud'
+)
+
